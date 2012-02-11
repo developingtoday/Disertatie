b9080dc3fa05bfd642a02c34c971a9a9210ad233
Revan <Revan@Revan-PC>
2012-02-11 17:18:01 +0200

    Layout complet, nu merge adaugat google maps-u

--- src/com/activities/SensorActivity.java
+++ src/com/activities/SensorActivity.java
@@ -1,7 +1,11 @@
 package com.activities;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,8 +15,55 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class SensorActivity extends Activity {
+    private TextView txtLatitude,txtLongitutde,txtOrientation,txtSpeed,txtDistance,txtAltitude;
+    private LocationManager lManager;
+    private LocationListener locListener;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensoractivity);
+        txtLatitude=(TextView)findViewById(R.id.txtLatitude);
+        txtLongitutde=(TextView)findViewById(R.id.txtLongitude);
+        txtOrientation=(TextView)findViewById(R.id.txtOrientation);
+        txtSpeed=(TextView)findViewById(R.id.txtSpeed);
+        txtDistance=(TextView)findViewById(R.id.txtDistance);
+        txtAltitude=(TextView)findViewById(R.id.txtAltitude);
+        lManager=(LocationManager)getSystemService(LOCATION_SERVICE);
+        locListener=new GpsLocationListener();
+        lManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locListener);
+    }
+
+
+    class GpsLocationListener implements LocationListener{
+
+        Double latitude, longitude,altitude;
+        Float speed;
+        @Override
+        public void onLocationChanged(Location location) {
+            //To change body of implemented methods use File | Settings | File Templates.
+            latitude=location.getLatitude();
+            longitude=location.getLongitude();
+            speed=location.getSpeed();
+            altitude=location.getAltitude();
+            txtLatitude.setText(latitude.toString());
+            txtLongitutde.setText(longitude.toString());
+            txtAltitude.setText(altitude.toString());
+            txtSpeed.setText(speed.toString());
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
     }
 }
