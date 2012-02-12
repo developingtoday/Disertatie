d5eb5a7ddbdb9bfdcfa66196ec381603dea54846
Revan <Revan@Revan-PC>
2012-02-12 12:30:24 +0200

    Serviciu web de vreme de la igoogle, inca nu s-a facut reverse geocodarea

--- src/com/activities/WeatherActivity.java
+++ src/com/activities/WeatherActivity.java
@@ -1,8 +1,13 @@
 package com.activities;
 
+import Obj.WeatherInfo;
+import Servicii.WeatherQueryWeb;
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 /**
  * Created by IntelliJ IDEA.
  * User: Revan
@@ -11,8 +16,39 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class WeatherActivity extends Activity {
+    private TextView txtCity,txtTemp,txtForecast;
+    private Button btnActualizeaza;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weatheractivity);
+        txtCity=(TextView)findViewById(R.id.txtCity);
+        txtForecast=(TextView)findViewById(R.id.txtForecast);
+        txtTemp=(TextView)findViewById(R.id.txtTemp);
+        btnActualizeaza=(Button)findViewById(R.id.btnGetWeather);
+        btnActualizeaza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ShowInfo();
+            }
+        });
+    }
+
+    private void ShowInfo() {
+        //TODO execute async
+        //TODO verificat cazul cand nu exista conexiune la internet
+        try{
+        WeatherQueryWeb q=new WeatherQueryWeb();
+            WeatherInfo         w=new WeatherInfo();
+        String url = "http://www.google.com/ig/api?weather=Constanta";
+        q.setUrl(url);
+        q.PopuleazaWeather();
+        w=q.getWeather();
+        txtCity.setText(w.getLocation());
+        txtTemp.setText(w.getTemp());
+        }catch(Exception ex)
+        {
+
+        }
     }
 }
---
+++ src/utils/Converter.java
@@ -0,0 +1,12 @@
+package utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Revan
+ * Date: 2/12/12
+ * Time: 10:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public  class Converter {
+
+}
